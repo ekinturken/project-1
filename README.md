2a8c98d4236f37e8731dedafa595945fe8acefed
Ekin Turken <ekinturken@ekins-mbp.wireless.newschool.edu>
2021-09-29 16:26:19 -0400

    project 1 committing

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 The starting point for a twitter bot, inspired by Christopher Strachey's love letter generator
 
 For the class "Radical Software", Eugene Lang College, Fall 2021
+
+I based my project on the current economic status of my home country, Turkey.
+The Turkish currency, the lira, is in rapid decline compared to the USD and
+sees fluctuation every second.
+Currently, 1 USD is worth 8.663 TRY, but this number is constantly changing.
+The cost of living in Turkey is almost entirely based on foreign currency, so in addition to the crazy inflation, the Turkish people also have to keep in mind the ever-changing currency rates.
+
+The former Turkish minister of economy, who is the son-in-law of the president, made fun of people complaining about the cost of living, saying: "Why do you care so much about the US Dollar? Do you get paid in dollars? If not, you shouldn't worry about it"
+Most of us do not get paid in USD, but we spend it everyday in grocery stores, pharmacies, cafes etc.
+
+The goal of my project is to highlight the fact that the amount of money we have changes within a day.
--- app/tutorial2.py
+++ app/tutorial2.py
@@ -1,3 +1,12 @@
+"""
+  A. Ekin Turken
+  Radical Software, Fall 2021
+  Project 1
+  Sept 23, 2021
+  python3
+"""
+
+
 import tweepy
 
 from authorization_tokens import *
@@ -22,35 +31,41 @@ message = ""
 
 
 #Option 3 = a list of possible mad libs
-#template_list = [ "My name is {} and I like {}.",
-#                  "People say I look like {} but only when I'm {}.",
-#                  "I think {} is the best at {}." ]
-#word_list1 = [ "Gritty", "Rory", "Nicolas Cage" ]
-#word_list2 = [ "playing hockey", "coding", "acting" ]
 
-#template = random.choice(template_list)
-#word1 = random.choice(word_list1)
-#word2 = random.choice(word_list2)
-#message = template.format(word1,word2)
+
 
 
 #Option4 = Logic
-string_template = "Hi there, I'm {}, master of {}."
+#string_template = "Hi there, I'm {}, master of {}."
 
-word_list1 = [ "Gritty", "Nicolas Cage" ]
-word_list2_a = [ "monsters", "playing hockey", "scaring people" ]
-word_list2_b = [ "movies", "acting", "saying 'woah'" ]
+#word_list1 = [ "Gritty", "Nicolas Cage" ]
+#word_list2_a = [ "monsters", "playing hockey", "scaring people" ]
+#word_list2_b = [ "movies", "acting", "saying 'woah'" ]
 
-if word1 == "Gritty":
-    word2 = random.choice(word_list2_a)
-elif word1 == "Nicolas Cage":
-    word2 = random.choice(word_list2_b)
+#if word1 == "Gritty":
+#    word2 = random.choice(word_list2_a)
+#elif word1 == "Nicolas Cage":
+#    word2 = random.choice(word_list2_b)
+
+#message = string_template.format(word1,word2)
+
+
 
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
+
+template_list = [ "Today you are {} poorer in TRY, {}.",
+                  "Today you are {} richer in TRY, {}." ]
+word_list1 = [ "0.6%", "0.54%", "0.73%", "0.22%", "0.66%", "0.94%", "3.2%", "2.4%", "1.1%" ]
+word_list2 = [ "yay!!", "too bad", "awesome!" ]
+
+template = random.choice(template_list)
+word1 = random.choice(word_list1)
+word2 = random.choice(word_list2)
+message = template.format(word1,word2)
 
 api.update_status(message)
 
